5f101890c613ff876696cbb1ec6de3745d038e7b
sriramvs25 <sriramvss25@gmail.com>
2025-09-09 23:31:46 -0400

    First commit

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+//Search Bar
+function Search() {
+  return (
+    <div>
+      <div class="searchbar">
+        <input type="text" placeholder="Search" />
+      </div>
+    </div>
+  );
+}
+
+export default Search
